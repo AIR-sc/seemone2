615c2f93a8ef2ab8d69b63b85ad83e358e283cc4
SC <simone.cuffaro@air-srl.com>
2020-11-05 12:31:56 +0100

    passwodoo views fix menu 2

--- passwodoo/controllers/controllers.py
+++ passwodoo/controllers/controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class MyModule(http.Controller):
-#     @http.route('/my_module/my_module/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class MyModule(http.Controller):
+    @http.route('/passwodoo/passwodoo/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/my_module/my_module/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('my_module.listing', {
-#             'root': '/my_module/my_module',
-#             'objects': http.request.env['my_module.my_module'].search([]),
-#         })
+    @http.route('/passwodoo/passwodoo/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('passwodoo.listing', {
+            'root': '/passwodoo/passwodoo',
+            'objects': http.request.env['passwodoo.passwodoo'].search([]),
+        })
 
-#     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('my_module.object', {
-#             'object': obj
-#         })
+    @http.route('/passwodoo/passwodoo/objects/<model("passwodoo.passwodoo"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('passwodoo.object', {
+            'object': obj
+        })
