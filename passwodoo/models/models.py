429d1d39e870ec342d46d72c895288b49205eaf9
SC <simone.cuffaro@air-srl.com>
2020-11-05 12:19:48 +0100

    passwodoo views fix menu

--- passwodoo/models/models.py
+++ passwodoo/models/models.py
@@ -11,6 +11,9 @@ class passwodoo(models.Model):
     expiringDate = fields.Date()
     password = fields.Char()
 
+    # Many 2 one verso il cliente
+    cliente = fields.Many2one('res.partner', string="Cliente")
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
